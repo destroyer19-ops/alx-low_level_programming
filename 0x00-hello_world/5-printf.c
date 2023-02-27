6a548143bf2ec4adaa3adcd1159f835d48060da8
destroyer19-ops <agamahalvin@gmail.com>
2023-02-27 15:18:55 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Description: C program to print string with printf
  *
  * Return: Always 0 (success)
- *
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

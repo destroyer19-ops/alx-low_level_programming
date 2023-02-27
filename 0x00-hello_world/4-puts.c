4a2e8c8a0e44806538df69b5c1ceae9f8669476f
destroyer19-ops <agamahalvin@gmail.com>
2023-02-27 15:02:50 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main: Printing
+ * main - Printing
  *
  * Description: C program to print using puts function
  *
  * Return: always 0 (success)
  */
-int main(void) {
-	puts("\"Programming is like a multilingual puzzle\"");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\"");
 	return (0);
 }

502aefe579d2386d95de4b8f016a1579b0a5afdd
destroyer19-ops <agamahalvin@gmail.com>
2023-02-27 14:55:47 -0800

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: Printing
+ *
+ * Description: C program to print using puts function
+ *
+ * Return: always 0 (success)
+ */
+int main(void) {
+	puts("\"Programming is like a multilingual puzzle\"");
+	return (0);
+}
